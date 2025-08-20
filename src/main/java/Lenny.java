03dc02464545656c4c88c073928cb59d98d8d3ce
Izzat <mizzat03@gmail.com>
2025-08-20 23:46:04 +0800

    Level-1

--- src/main/java/Lenny.java
+++ src/main/java/Lenny.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+
 public class Lenny {
     public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in); //read input
+
+        String terminatingLine = "Bye. Hope to see you again soon!";
+        String terminatingCondition = "bye";
+
         String firstline = "Hello! I'm Lenny!";
         String secondline = "What can I do for you?";
-        String thirdline = "Bye. Hope to see you again soon!";
-        System.out.println(firstline+ "\n" +secondline+ "\n" + thirdline);
+
+        System.out.println(firstline + "\n" + secondline);
+
+        String input = myObj.nextLine();
+
+        while (!input.equals(terminatingCondition)) {
+            System.out.println(input);
+            input = myObj.nextLine();
+        }
+
+        System.out.println(terminatingLine);
     }
+
 }
