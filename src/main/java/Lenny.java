731b6b18745899a0e5777f269390d038b97b63e8
Izzat <mizzat03@gmail.com>
2025-08-20 23:22:56 +0800

    Level-0

---
+++ src/main/java/Lenny.java
@@ -0,0 +1,8 @@
+public class Lenny {
+    public static void main(String[] args) {
+        String firstline = "Hello! I'm Lenny!";
+        String secondline = "What can I do for you?";
+        String thirdline = "Bye. Hope to see you again soon!";
+        System.out.println(firstline+ "\n" +secondline+ "\n" + thirdline);
+    }
+}
