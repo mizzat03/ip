89a6e289133e74ea6b93a9b3565c2a6deb0c426a
Izzat <mizzat03@gmail.com>
2025-08-30 13:06:45 +0800

    Added JUnit tests, run with Gradle

---
+++ src/test/java/storage/StorageTest.java
@@ -0,0 +1,36 @@
+package storage;
+
+import lenny.task.Task;
+import lenny.task.Todo;
+import lenny.task.Deadline;
+import lenny.storage.Storage;
+import lenny.task.TaskList;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StorageTest {
+
+    @Test
+    void saveAndLoad_roundTrip_preservesTasks() throws Exception {
+        Path tempFile = Files.createTempFile("tasks", ".txt");
+        Storage storage = new Storage(tempFile.toString());
+        storage.ensureFile();
+
+        TaskList tasks = new TaskList();
+        tasks.add(new Todo("test1", false));
+        tasks.add(new Deadline("report", "2025-09-01", true));
+
+        storage.save(tasks);
+        ArrayList<Task> reloaded = storage.load();
+
+        assertEquals(2, reloaded.size());
+        assertEquals("test1", reloaded.get(0).getTaskName());
+        assertTrue(reloaded.get(1).getIsDone());
+    }
+}
